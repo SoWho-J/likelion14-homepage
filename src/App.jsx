4e7537f306508c174b32608f1763ffc0c3643f1e
sowho.j <jangseohu112@gmail.com>
2026-05-19 18:50:46 +0900

    ✨ Feat: 초기 뼈대 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // react-router-dom에서 라우팅에 필요한 기능을 가져오는 부분.
+import RootLayout from "../src/layout/RootLayout.jsx"; // 컴포넌트들 가져오기.
+import Main from "../src/pages/Main/Main.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // 앱의 중심컴포넌트
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    // 화면에 보여줄 중심 컴포넌트 반환
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          {/* 이 아래 모든 페이지는 RootLayout을 공통으로 사용하겠다는 의미*/}
+          <Route path="/" element={<Main />} />
+          {/* 메인 호출. 화면은 메인에서 시작, 주소가 /이면 메인페이지를 보여줘*/}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
+//BrowserRouter가 주소창 변화를 감지 -> Routes가 여러 route중에서 현재 주소와 맞는 공간을 찾음 -> 현재 주소와 맞으면 route는 현재 주소에 맞는 컴포넌트를 가져옴
+export default App;
+//App 컴포넌트를 다른 파일에서 사용할 수 있게 내보내는 코드.
---
+++ src/layout/Routlayout.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom"; //전체페이지의 공통 구조를 만드는 컴포넌트 outlet에 자식컴포넌트가 들어감 메인에서는 Main이 outlet에 들어감
+import Header from "../components/header/Header"; //헤더 컴포넌트 불러오기
+
+export default function RootLayout() {
+  return (
+    <>
+      <Header /> {/*헤더*/}
+      <main>
+        <Outlet />{" "}
+        {/*App.jsx에서 Root Layout 하위에 있던 Main 페이지가 설정한 경로에 따라 <Out let> 자리에 들어간다*/}
+      </main>
+    </>
+  );
+}
