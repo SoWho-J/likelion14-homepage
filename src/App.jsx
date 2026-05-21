85b951969293037805387ddeb8dde72f61f0ce31
sowho.j <jangseohu112@gmail.com>
2026-05-22 06:07:24 +0900

    ✨ Feat: 페이지 생성에 따른 구조변경 및 라우트 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"; // react-router-dom에서 라우팅에 필요한 기능을 가져오는 부분.
-import RootLayout from "../src/layout/RootLayout.jsx"; // 컴포넌트들 가져오기.
-import Members from "../src/pages/main/Members.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RootLayout from "./layout/Rootlayout";
+import Members from "./pages/main/Members";
+import Login from "./pages/Login/Login";
+import Project from "./pages/Project/Project";
+import Recruit from "./pages/recruit/Recruit";
 
 function App() {
-  // 앱의 중심컴포넌트
   return (
-    // 화면에 보여줄 중심 컴포넌트 반환
     <BrowserRouter>
       <Routes>
         <Route element={<RootLayout />}>
-          {/* 이 아래 모든 페이지는 RootLayout을 공통으로 사용하겠다는 의미*/}
           <Route path="/" element={<Members />} />
-          {/* 메인 호출. 화면은 메인에서 시작, 주소가 /이면 메인페이지를 보여줘*/}
+          <Route path="/members" element={<Members />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/recruit" element={<Recruit />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-//BrowserRouter가 주소창 변화를 감지 -> Routes가 여러 route중에서 현재 주소와 맞는 공간을 찾음 -> 현재 주소와 맞으면 route는 현재 주소에 맞는 컴포넌트를 가져옴
+
 export default App;
-//App 컴포넌트를 다른 파일에서 사용할 수 있게 내보내는 코드.
--- src/layout/Rootlayout.jsx
+++ src/layout/Rootlayout.jsx
@@ -1,14 +1,26 @@
-import { Outlet } from "react-router-dom"; //전체페이지의 공통 구조를 만드는 컴포넌트 outlet에 자식컴포넌트가 들어감 메인에서는 Main이 outlet에 들어감
-import Header from "../components/header/Header"; //헤더 컴포넌트 불러오기
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
 
 export default function RootLayout() {
   return (
-    <>
-      <Header /> {/*헤더*/}
-      <main>
+    <Page>
+      <Header />
+      <Main>
         <Outlet />
-        {/*App.jsx에서 Root Layout 하위에 있던 Main 페이지가 설정한 경로에 따라 <Out let> 자리에 들어간다*/}
-      </main>
-    </>
+      </Main>
+      <Footer />
+    </Page>
   );
 }
+
+const Page = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.main`
+  flex: 1;
+`;
